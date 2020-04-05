b1e4106460967159306b86dcfdca8bba64afd801
WCZ <wcz0@outlook.com>
2020-04-06 00:27:55 +0800

    update files

--- WebSite/application/index/controller/Index.php
+++ WebSite/application/index/controller/Index.php
@@ -7,37 +7,53 @@ class Index extends Controller
 {
     public function index()
     {
+
+        $this->assign("index","1");
+        $this->assign("news","0");
+        $this->assign("picture","0");
+        $this->assign("about","0");
+        $this->assign("contact","0");
         return $this->fetch();
 
     }
     
     public function news()
     {
+        $this->assign("index","0");
+        $this->assign("news","1");
+        $this->assign("picture","0");
+        $this->assign("about","0");
+        $this->assign("contact","0");
         return $this->fetch();
     }
 
     public function about()
     {
+        $this->assign("index","0");
+        $this->assign("news","0");
+        $this->assign("picture","0");
+        $this->assign("about","1");
+        $this->assign("contact","0");
         return $this->fetch();
     }
 
     public function contact()
     {
+        $this->assign("index","0");
+        $this->assign("news","0");
+        $this->assign("picture","0");
+        $this->assign("about","0");
+        $this->assign("contact","1");
         return $this->fetch();
     }
 
     public function picture()
     {
-        return $this->fetch();
-    }
-
-    public function login()
-    {
-        return $this->fetch();
-    }
-
-    public function register()
-    {
+        $this->assign("index","0");
+        $this->assign("news","0");
+        $this->assign("picture","1");
+        $this->assign("about","0");
+        $this->assign("contact","0");
         return $this->fetch();
     }
 
