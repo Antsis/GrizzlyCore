bca1436bcb985a4e009c2c8dde57a6f62c0994db
wcz0 <wcz0@outlook.com>
2020-11-17 14:57:34 +0800

    commit modfi

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [IndexController::class, 'index']);
+Route::get('about', [IndexController::class, 'about']);
+Route::get('bbs', [IndexController::class, 'bbs']);
+Route::get('contact', [IndexController::class, 'contact']);
+
+Route::get('logout', [IndexController::class, 'logout']);
+Route::any('checkLogined', [IndexController::class, 'checkLogined']);
+Route::any('updateData', [IndexController::class, 'updateData']);
+Route::get('qqcallback', [LoginController::class, 'qqcallback']);
+
+
+Route::prefix('login')->group(function(){
+    Route::post('cookieLogin', [LoginController::class, 'cookieLogin']);
+    Route::post('checkCode', [LoginController::class, 'checkCode']);
+    Route::post('register', [LoginController::class, 'register']);
+    Route::post('register2', [LoginController::class, 'register2']);
+    Route::post('sendCodeSmsEmail', [LoginController::class, 'sendCodeSmsEmail']);
+    Route::post('login', [LoginController::class, 'login']);
+    Route::get('qqLogin', [LoginController::class, 'qqLogin']);
+    Route::get('verify', [LoginController::class, 'verify']);
+
+});
+
+Route::middleware('checksession')->prefix('profile')->group(function(){
+    Route::get('profile/{op?}', [ProfileController::class, 'profile']);
+    Route::get('avatar', [ProfileController::class, 'avatar']);
+    Route::get('account', [ProfileController::class, 'account']);
+    Route::post('avatarUpload', [ProfileController::class, 'avatarUpload']);
+    Route::get('avatar', [ProfileController::class, 'avatar']);
+    Route::get('avatar', [ProfileController::class, 'avatar']);
+    Route::get('avatar', [ProfileController::class, 'avatar']);
 });
