af99eb69e4402d94f2cfb16011b8babaf982d7fe
wcz0 <wcz0@outlook.com>
2021-02-28 20:29:20 +0800

    帖子初尝试

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,13 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Category extends Model
 {
     use HasFactory;
-    use SoftDeletes;
-    protected $dateFormat = 'U';
-
 }
--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -17,6 +17,7 @@ Grizzly
             <li>电商功能</li>
             <li>聊天系统</li>
             <li>图片上传的太卡, 会造成浏览器性能问题, 每次调取头像时, php读取头像分辨率过大会造成性能问题, 导航栏需要静态头像资源, 上传到服务器的头像需要进行压缩</li>
+            <li>上传图片后使用队列处理</li>
         </ul>
         <a class="btn btn-primary btn-lg" href="vscode:" role="button">Just do it</a>
       </div>
