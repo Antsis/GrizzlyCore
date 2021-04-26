4b71f5b78a1baf37231a974c8c9e3b4ce120e83a
wcz0 <wcz0@outlook.com>
2021-04-27 00:41:26 +0800

    modif

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+// TODO:webpack配置还没完成
 const config = {
   target: "web",
   entry: path.join(__dirname, "src/index.js"),
@@ -13,4 +17,10 @@ const config = {
       },
     ],
   },
-};
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
+
+
+module.exports = config
